557c76995f6a8e772d994a5f98b5dffeaee495d7
Filip <f2alhama@uwaterloo.ca>
2021-09-22 19:43:18 -0400

    finished quiz functionality, highscore redirecting, and the all done page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,14 @@ let quiz = document.querySelector('.Quiz')
 let highScore = document.querySelector('.HighScorePage')
 let donePage = document.querySelector('.DonePage')
 let main = document.querySelector('.main-page')
+let question1 = document.querySelector('.question1')
+let question2 = document.querySelector('.question2')
+let question3 = document.querySelector('.question3')
+let question4 = document.querySelector('.question4')
+let score = document.querySelector('.FinalScore')
 
-startQuizButton.addEventListener("click", runTimer)
-let count = 3
+let hsRedirect = document.querySelector("a")
+let mainRedirect = document.querySelector(".ReturnToMenu")
 
 function hideMain() {
     main.style.display = "none"
@@ -42,19 +47,75 @@ function showDone() {
     donePage.style.display = "flex"
 }
 
+function hideQ1() {
+    question1.style.display = "none"
+}
+
+function showQ1() {
+    question1.style.display = "flex"
+}
+
+function hideQ2() {
+    question2.style.display = "none"
+}
+
+function showQ2() {
+    question2.style.display = "flex"
+}
+
+function hideQ3() {
+    question3.style.display = "none"
+}
+
+function showQ3() {
+    question3.style.display = "flex"
+}
+
+function hideQ4() {
+    question4.style.display = "none"
+}
+
+function showQ4() {
+    question4.style.display = "flex"
+}
+
 hideQuiz()
 hideHighScore()
 hideDone()
+hideQ1()
+hideQ2()
+hideQ3()
+hideQ4()
 
-function runTimer() {
-    timeLeft.textContent = 3
+function redirectHs(event) {
+    event.preventDefault()
+    hideMain()
+    hideDone()
+    showHighScore()
+}
+
+function redirectMain(event) {
+    event.preventDefault()
+    hideHighScore()
+    showMain()
+}
+
+startQuizButton.addEventListener("click", runTimer)
+hsRedirect.addEventListener("click", redirectHs)
+mainRedirect.addEventListener("click", redirectMain)
+
+let count = 75
+let finalScore = "" 
+
+function runTimer(event) {
+    event.preventDefault()
     runGame()
 
     let timeInterval = setInterval(function () {
         count--;
         timeLeft.textContent = count
 
-        if (count == 0) {
+        if ((count <= 0) || (finalScore != "")) {
             clearInterval(timeInterval)
             hideQuiz()
             showDone()
@@ -67,8 +128,125 @@ function runGame() {
     hideMain()
     showQuiz()
     let questions = {
-        "how old are you?": [23, 24, 25, 26],
-        "where were you born?": ['serbia', 'canada', 'america', 'china']
+        1: ["HTML", "CSS", "JavaScript", "kotlin"],
+        2: ['flex', 'True', 'inline-flex', 'hidden'],
+        3: ["styling", "markup", "scripting", "Database"],
+        4: [".length()", ".push()", ".querySelector()", ".addEventListener()"],
     }
-    console.log(questions)
+
+    function Question1() {
+        hideMain()
+        showQ1()
+        question1.textContent = "Which tool is NOT one of the primary three web developers learn?"
+
+        let questionsList1 = questions[1]
+
+        let answer1 = "kotlin"
+
+        for (j = 0; j < 4; j++) {
+            let button1 = document.createElement("BUTTON");
+            let text1 = document.createTextNode(questionsList1[j]);
+            button1.appendChild(text1)
+            question1.appendChild(button1)
+        }
+
+        question1.addEventListener("click", function(event){
+            let buttonClicked1 = event.target.textContent
+            if (buttonClicked1 == answer1) {
+                return Question2()
+            } else {
+                count = count - 10
+                return Question2()
+            }
+        })
+        
+    }
+    Question1()
+
+    function Question2() {
+        hideQ1()
+        showQ2()
+        question2.textContent = "Which of these are NOT a CSS display type?"
+
+        let questionsList2 = questions[2]
+
+        let answer2 = "True"
+
+        for (j = 0; j < 4; j++) {
+            let button2 = document.createElement("BUTTON");
+            let text2 = document.createTextNode(questionsList2[j]);
+            button2.appendChild(text2)
+            question2.appendChild(button2)
+        }
+
+        question2.addEventListener("click", function(event){
+            let buttonClicked2 = event.target.textContent
+            if (buttonClicked2 == answer2) {
+                Question3()
+            } else {
+                count = count - 10
+                Question3()
+            }
+        })
+        
+    }
+
+    function Question3() {
+        hideQ2()
+        showQ3()
+        question3.textContent = "JavaScript is defined as what kind of a language?"
+
+        let questionsList3 = questions[3]
+
+        let answer3 = "scripting"
+
+        for (j = 0; j < 4; j++) {
+            let button3 = document.createElement("BUTTON");
+            let text3 = document.createTextNode(questionsList3[j]);
+            button3.appendChild(text3)
+            question3.appendChild(button3)
+        }
+
+        question3.addEventListener("click", function(event){
+            let buttonClicked3 = event.target.textContent
+            if (buttonClicked3 == answer3) {
+                Question4()
+            } else {
+                count = count - 10
+                Question4()
+            }
+        })
+        
+    }
+
+    function Question4() {
+        hideQ3()
+        showQ4()
+        question4.textContent = "which of these is NOT a method in JavaScript?"
+
+        let questionsList4 = questions[4]
+
+        let answer4 = ".length()"
+
+        for (j = 0; j < 4; j++) {
+            let button4 = document.createElement("BUTTON");
+            let text4 = document.createTextNode(questionsList4[j]);
+            button4.appendChild(text4)
+            question4.appendChild(button4)
+        }
+
+        question4.addEventListener("click", function(event){
+            let buttonClicked4 = event.target.textContent
+            if (buttonClicked4 == answer4) {
+                finalScore = count - 1 
+                score.textContent = "Your final score was: " + String(finalScore)
+            } else {
+                count = count - 10
+                finalScore = count - 1 
+                score.textContent = "Your final score was: " + String(finalScore)
+            } return finalScore
+        })
+    }
 }
+
+
