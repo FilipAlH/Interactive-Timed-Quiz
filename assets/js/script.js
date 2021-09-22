1e3dd9fd49277bbad71ea3ad3fb4d195d6089da1
Filip <f2alhama@uwaterloo.ca>
2021-09-22 02:14:11 -0400

    defined functions for hiding and showing pages, got rungame function to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,74 @@
 //timer function with start button event listener
 
-let startQuizButton = document.querySelector("#start-quiz")
+let startQuizButton = document.querySelector('.start-quiz')
+let timeLeft = document.querySelector('.Timer');
+let quiz = document.querySelector('.Quiz')
+let highScore = document.querySelector('.HighScorePage')
+let donePage = document.querySelector('.DonePage')
+let main = document.querySelector('.main-page')
 
-startQuizButton.addEventListener("click", function(event) {
-    event.stopPropagation()
+startQuizButton.addEventListener("click", runTimer)
+let count = 3
+
+function hideMain() {
+    main.style.display = "none"
+}
+
+function showMain() {
+    main.style.display = "flex"
+}
+
+function hideQuiz() {
+    quiz.style.display = "none"
+}
+
+function showQuiz() {
+    quiz.style.display = "flex"
+}
+
+function hideHighScore() {
+    highScore.style.display = "none"
+}
+
+function showHighScore() {
+    highScore.style.display = "flex"
+}
+
+function hideDone() {
+    donePage.style.display = "none"
+}
+
+function showDone() {
+    donePage.style.display = "flex"
+}
+
+hideQuiz()
+hideHighScore()
+hideDone()
+
+function runTimer() {
+    timeLeft.textContent = 3
     runGame()
-});
 
-function runGame() {
-    count = 75
-    let timeLeft = document.querySelector(".Timer")
-    
-    let timeInterval = setInterval(function() {
+    let timeInterval = setInterval(function () {
         count--;
         timeLeft.textContent = count
 
-        if (count === 0) {
+        if (count == 0) {
             clearInterval(timeInterval)
-        }
-    })
+            hideQuiz()
+            showDone()
+
+        } 
+    }, 1000);
 }
 
+function runGame() {
+    hideMain()
+    showQuiz()
+    let questions = {
+        "how old are you?": [23, 24, 25, 26],
+        "where were you born?": ['serbia', 'canada', 'america', 'china']
+    }
+    console.log(questions)
+}
